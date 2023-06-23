e8bbdbf2d446b69b66f4d9b3df3d33cd02c890b3
Prakash Kumar <132866106+PrakashDevnDsa@users.noreply.github.com>
2023-06-23 15:50:44 +0530

    Add files via upload

---
+++ GPT-3/src/containers/blog/import.js
@@ -0,0 +1,14 @@
+import blog1 from '../../assets/1.png'
+import blog2 from '../../assets/2.png'
+import blog3 from '../../assets/3.png'
+import blog4 from '../../assets/4.png'
+import blog5 from '../../assets/5.png'
+
+
+export {
+    blog1,
+    blog2,
+    blog3,
+    blog4,
+    blog5,
+}
